06fcf097e4d22b83feac74dcb8649f279ba60113
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2019-11-06 23:29:47 +0000

    Scale the Lander

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const MARS_g = 3.7;
+const LANDER_SCALE = 1.0/150.0;
 
 var clock = new THREE.Clock()
 var scene = new THREE.Scene();
@@ -9,22 +10,22 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.physicallyCorrectLights = true;
 document.body.appendChild(renderer.domElement);
 
-camera.position.y = 100;
-camera.position.z = 500;
+camera.position.y = 2;
+camera.position.z = 2;
 camera.lookAt(0, 0, 0);
 
-var light = new THREE.AmbientLight( 0x404040, 10 ); // soft white light
-scene.add( light );
+var light = new THREE.AmbientLight(0x404040, 10); // soft white light
+scene.add(light);
 
 var loader = new THREE.GLTFLoader();
 var lander;
-var landerMass = 290;
+var landerMass = 290 + 64;
 var landerVector = new THREE.Vector3()
 
 function main_loop() {
 	t = clock.getDelta();
 
-	landerVector.y -= MARS_g * landerMass * 0.0001;
+	landerVector.y -= MARS_g * landerMass * 0.00001;
 	lander.position.y += landerVector.y;
 
 	camera.lookAt(lander.position.x, lander.position.y, lander.position.z);
@@ -41,6 +42,9 @@ function start_main_loop() {
 loader.load('models/mars-polar-lander/scene.gltf', function (gltf) {
 	lander = gltf.scene;
 	scene.add(lander);
+	lander.scale.x = LANDER_SCALE;
+	lander.scale.y = LANDER_SCALE;
+	lander.scale.z = LANDER_SCALE;
 	start_main_loop();
 }, undefined, function (error) {
 	console.error(error);
