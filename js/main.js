4d5b0a86b596b82f6d1d747f7c9f592af356c07b
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2019-11-06 22:55:03 +0000

    Add some gravity

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const MARS_g = 3.7;
+
+var clock = new THREE.Clock()
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -6,8 +9,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.physicallyCorrectLights = true;
 document.body.appendChild(renderer.domElement);
 
-camera.position.y = 200;
-camera.position.z = 300;
+camera.position.y = 100;
+camera.position.z = 500;
 camera.lookAt(0, 0, 0);
 
 var light = new THREE.AmbientLight( 0x404040, 10 ); // soft white light
@@ -15,16 +18,32 @@ scene.add( light );
 
 var loader = new THREE.GLTFLoader();
 var lander;
-loader.load('models/mars-polar-lander/scene.gltf', function (gltf) {
-	lander = gltf.scene;
-	scene.add(lander);
-}, undefined, function ( error ) {
-	console.error( error );
-});
+var landerMass = 290;
+var landerVector = new THREE.Vector3()
 
-function animate() {
-	requestAnimationFrame(animate);
+function main_loop() {
+	t = clock.getDelta();
+
+	landerVector.y -= MARS_g * landerMass * 0.0001;
+	lander.position.y += landerVector.y;
+
+	camera.lookAt(lander.position.x, lander.position.y, lander.position.z);
+
+	requestAnimationFrame(main_loop);
 	renderer.render(scene, camera);
 }
 
-animate();
+function start_main_loop() {
+	clock.getDelta();
+	main_loop();
+}
+
+loader.load('models/mars-polar-lander/scene.gltf', function (gltf) {
+	lander = gltf.scene;
+	scene.add(lander);
+	start_main_loop();
+}, undefined, function (error) {
+	console.error(error);
+});
+
+
