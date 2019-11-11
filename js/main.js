982930c40144bcfebb1842e500447abdfde0a64a
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2019-11-11 22:45:57 +0000

    Add Mars' surface and more lighting

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
-const MARS_g = 3.7; // Acceleration due to gravity near Mars' surface
-const LANDER_SCALE = 1.0/150.0; // At this scale 1 unit is approx. equal to 1 meter
+const MARS_g = 3.7; // Acceleration due to gravity near Mars' surface.
+const LANDER_SCALE = 1.0/150.0; // At this scale 1 unit of length is approx. equal to 1 meter.
 const LANDER_MASS = 290 + 64; // 290 kg for Lander itself and 64 kg of propellant.
-const TIME_SCALE = 0.01;
+const TIME_SCALE = 0.1;
+const CLIP_DISTANCE = 1000;
+
+function initMarsSurface() {
+	var geometry = new THREE.PlaneGeometry(CLIP_DISTANCE + 100, CLIP_DISTANCE + 100, 1, 1);
+	var texture = new THREE.TextureLoader().load('textures/brown-and-black-crater-87655.jpg');
+	var material = new THREE.MeshStandardMaterial({color: 0xffdddd, map: texture});
+	var plane = new THREE.Mesh(geometry, material);
+	return plane;
+}
 
 var clock = new THREE.Clock()
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, CLIP_DISTANCE);
+var surface = initMarsSurface();
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.physicallyCorrectLights = true;
 document.body.appendChild(renderer.domElement);
 
-camera.position.y = -10;
-camera.position.z = 2;
-camera.lookAt(0, 0, 0);
+var hemisphereLight = new THREE.HemisphereLight(0xffeeee, 0x666655, 2.0);
+scene.add(hemisphereLight);
 
-var light = new THREE.AmbientLight(0x404040, 10); // soft white light
-scene.add(light);
+var directionalLight = new THREE.DirectionalLight(0xffeeee, 0.5);
+scene.add(directionalLight);
+
+
+var ambientLight = new THREE.AmbientLight(0x404040, 3.0); // soft white light
+scene.add(ambientLight);
+
+scene.add(surface);
 
 var lander;
 var landerVelocity = new THREE.Vector3()
@@ -46,7 +61,21 @@ function mainLoop() {
 	renderer.render(scene, camera);
 }
 
+function setInitialPositions() {
+	directionalLight.position.x = -(2*CLIP_DISTANCE);
+	directionalLight.position.z = -(2*CLIP_DISTANCE);
+	directionalLight.position.y = 4*CLIP_DISTANCE;
+
+	lander.position.y = 500;
+
+	camera.position.y = lander.position.y - 10;
+	camera.position.z = 3;
+
+	surface.rotateX(- Math.PI / 2.0);
+}
+
 function startMainLoop() {
+	setInitialPositions();
 	clock.getDelta();
 	mainLoop();
 }
---
+++ textures/README.txt
@@ -0,0 +1,1 @@
+brown-and-black-crater-87655.jpg from https://www.pexels.com/photo/brown-and-black-crater-87655/
