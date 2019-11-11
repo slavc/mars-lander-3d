0e0f1519be973f9727d576fc9cae8c7aa2e78823
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2019-11-11 21:35:11 +0000

    Refactor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const MARS_g = 3.7; // Acceleration due to gravity near Mars' surface
 const LANDER_SCALE = 1.0/150.0; // At this scale 1 unit is approx. equal to 1 meter
+const LANDER_MASS = 290 + 64; // 290 kg for Lander itself and 64 kg of propellant.
 const TIME_SCALE = 0.01;
 
 var clock = new THREE.Clock()
@@ -18,37 +19,47 @@ camera.lookAt(0, 0, 0);
 var light = new THREE.AmbientLight(0x404040, 10); // soft white light
 scene.add(light);
 
-var loader = new THREE.GLTFLoader();
 var lander;
-var landerMass = 290 + 64;
-var landerVector = new THREE.Vector3()
+var landerVelocity = new THREE.Vector3()
 
-function main_loop() {
+var gravity = new THREE.Vector3(0, -MARS_g, 0);
+
+function addForces(t) {
+	landerVelocity.y += gravity.y * t*TIME_SCALE;
+}
+
+function updatePosition() {
+	lander.position.x += landerVelocity.x;
+	lander.position.y += landerVelocity.y;
+	lander.position.z += landerVelocity.z;
+}
+
+function mainLoop() {
 	t = clock.getDelta();
 
-	landerVector.y -= MARS_g * t * TIME_SCALE;
-	lander.position.y += landerVector.y;
+	addForces(t);
+	updatePosition();
 
 	camera.lookAt(lander.position.x, lander.position.y, lander.position.z);
 
-	requestAnimationFrame(main_loop);
+	requestAnimationFrame(mainLoop);
 	renderer.render(scene, camera);
 }
 
-function start_main_loop() {
+function startMainLoop() {
 	clock.getDelta();
-	main_loop();
+	mainLoop();
 }
 
+var loader = new THREE.GLTFLoader();
 loader.load('models/mars-polar-lander/scene.gltf', function (gltf) {
 	lander = gltf.scene;
 	scene.add(lander);
 	lander.scale.x = LANDER_SCALE;
 	lander.scale.y = LANDER_SCALE;
 	lander.scale.z = LANDER_SCALE;
-	start_main_loop();
+	startMainLoop();
 }, undefined, function (error) {
 	console.error(error);
 });
-
 
